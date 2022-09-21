6327bb3510a73e995356b6c58f8ff070e31545e6
Aroweeri <dylanlsutton@gmail.com>
2022-09-20 21:17:51 -0400

    Ball grows while mouse is held, mass grows proportionally to the size.

---
+++ Ball.gd
@@ -0,0 +1,12 @@
+extends RigidBody2D
+
+var growing = true;
+export var growthSpeed = 1;
+
+
+func _process(delta):
+	if(growing):
+		$CollisionShape2D.scale.x += growthSpeed * delta;
+		$CollisionShape2D.scale.y += growthSpeed * delta;
+		mass = 1 * $CollisionShape2D.scale.x * $CollisionShape2D.scale.x;
+		print(mass);
--- Main.gd
+++ Main.gd
@@ -1,21 +1,37 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var pressed = false;
+var clickPos = null;
+var ball = preload("res://Ball.tscn")
+var newBall;
+var clickTime = 0;
+var releaseTime = 0;
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+	
+	
+func _unhandled_input(event):
+	if(event is InputEventMouseButton):
+		if(event.button_index == BUTTON_LEFT and not event.pressed):
+			if(clickPos):
+				releaseTime = OS.get_ticks_msec();
+				print(releaseTime);
+				newBall.growing = false;
+				newBall.apply_central_impulse(Vector2(-1000*newBall.mass,0));
+				clickPos = null;
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-func _input(event):
-	if(event.is_action_pressed("ui_accept")):
-		#$Ball.sleeping = false;
-		$Ball.apply_central_impulse(Vector2(4000,0));
+func _on_Area2D_input_event(viewport, event, shape_idx):
+	if(not event is InputEventMouseButton):
+		return;
+		
+	if(event.button_index == BUTTON_LEFT and event.pressed):
+		clickPos = event.position;
+		clickTime = OS.get_ticks_msec();
+		print(clickTime);
+		newBall = ball.instance();
+		newBall.position = event.position;
+		add_child(newBall);
